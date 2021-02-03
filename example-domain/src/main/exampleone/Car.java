9db594e1e41b803135dab9a7f47ce0f5ca58c5e9
Max <laut-max@mail.ru>
2021-02-03 16:26:26 +0300

    new Car

---
+++ example-domain/src/main/exampleone/Car.java
@@ -0,0 +1,13 @@
+package main.exampleone;
+
+/**
+ * Created by Max.B on 03.02.2021.
+ */
+public class Car {
+
+    private String engine;
+    private String transmission;
+
+    public Car(){}
+
+}
